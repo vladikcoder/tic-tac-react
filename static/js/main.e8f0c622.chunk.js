ecc4b68fe3adb32fbfd14a321d80688b555974b9
Vladik <optimus014@gmail.com>
2019-02-21 20:36:09 +0200

    Updates

---
+++ static/js/main.e8f0c622.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{10:function(n,e,t){"use strict";t.r(e);var o=t(0),c=t.n(o),r=t(2),a=t.n(r),u=(t(9),function(){return c.a.createElement("div",{className:"App"})});a.a.render(c.a.createElement(u,null),document.getElementById("root"))},3:function(n,e,t){n.exports=t(10)},9:function(n,e,t){}},[[3,1,2]]]);
+//# sourceMappingURL=main.e8f0c622.chunk.js.map
